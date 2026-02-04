b46420abab7e1b0bad8f5b36ef41e453757fe2fa
nick-dil <nick.dillen@inbo.be>
2026-02-04 18:19:42 +0100

    initial commit

---
+++ tests/testthat/test-sample_data.R
@@ -0,0 +1,8 @@
+test_that("read.sample_sheet_lab can read the template gsheet", {
+  expect_error(
+    read.sample_sheet_lab(
+      "1cWkVqk3y7668OVRhIlpa0fUmTh4oJTRWUqp60GRJ5J8"
+    ))
+})
+
+### Add real test when/if template public
