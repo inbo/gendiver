8e8173fdb1e455abf3bb7e83b8adfc325f770959
Nick Dillen <47636003+nick-dil@users.noreply.github.com>
2026-02-05 13:10:23 +0100

    prep ps

--- R/taxonomy.R
+++ R/taxonomy.R
@@ -146,22 +146,27 @@ parse_tax_string = function(x, is_pred=F){
 #' @examples
 #' #To add
 read.taxonomy_sintax = function(sintax_path, read_supported_only=TRUE, add_source=FALSE, clean_tax=FALSE){
-  rtab = read.table_gdrive(sintax_path, sep = "\t", header = F, col.names = c("QUERY_LABEL","TAX_PRED", "STRAND", "TAX_PRED_CUTOFF" ))
+  # Read input
+  rtab = read.table_gdrive(
+    sintax_path, sep = "\t", header = F,
+    col.names = c("QUERY_LABEL","TAX_PRED", "STRAND", "TAX_PRED_CUTOFF")
+    )
 
   # order the input on string length to increase chance of full taxonomy template
   rtab = rtab[order(nchar(rtab$TAX_PRED_CUTOFF), decreasing = T),]
-  if (read_supported_only){
+  if (read_supported_only) {
     ## Read supported
     tax_supp = data.table::rbindlist(lapply(rtab$TAX_PRED_CUTOFF, parse_tax_string), fill = T)
   } else {
     ## Read complete prediction
-    tax_supp = data.table::rbindlist(lapply(rtab$TAX_PRED, parse_tax_string, is_pred=T), fill = T)
+    tax_supp = data.table::rbindlist(lapply(rtab$TAX_PRED, parse_tax_string, is_pred = T), fill = T)
   }
 
-  tax_supp = cbind(rtab[1], tax_supp)
+  # Add OTU-IDs back to data
+  row.names(tax_supp) = rtab$QUERY_LABEL
 
-  if (clean_tax){tax_supp = clean_taxonomies(tax_supp)}
-  if (add_source){tax_supp$SOURCE_FILE = basename(sintax_path)}
+  if (clean_tax) {tax_supp = clean_taxonomies(tax_supp)}
+  if (add_source) {tax_supp$SOURCE_FILE = basename(sintax_path)}
 
   return(tax_supp)
 }
@@ -193,7 +198,7 @@ read.taxonomy_idtaxa = function(tax_tab_path, read_supported_only=TRUE, add_sour
 
   tabraw$QUERY_LABEL = gsub(pattern = "\t.*", "", tabraw$QUERY_LABEL)
 
-  if (read_supported_only){
+  if (read_supported_only) {
     # Remove predictions
     tabraw[taxranks] = lapply(tabraw[taxranks], gsub, pattern = "\\(\\d+\\.?\\d+%*\\)", replacement = "")
     # remove unclassified taxonomies
