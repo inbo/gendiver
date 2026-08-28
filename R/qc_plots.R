06172b105e94e05ff0a3073853fe103de5bb2ad3
Nick Dillen <47636003+nick-dil@users.noreply.github.com>
2026-08-28 18:26:46 +0200

    add tile marking ANALYSE_TYPE

--- R/qc_plots.R
+++ R/qc_plots.R
@@ -215,22 +215,31 @@ qcplot.plate_heatmap_toptaxa = function(ps_obj, omit_cutoff = 100){
 #' @param ps_obj Phyloseq object as INPUT
 #' @param omit_cutoff integer, omit samples with less than n reads (default=100). This is important for color gradient to make sense.
 #' @param transform (default="identity") See \link[ggplot2]{scale_continuous}, e.g. "log2", "log10", "sqrt", ...
+#' @param mark_controls (default=FALSE) Mark tiles based on ANALYSE_TYPE. Only non MB data.
+#' @param mark_data (default=FALSE) Mark tiles based on ANALYSE_TYPE. All experiment/MB data.
+#' @param all_wells (default=FALSE) Plot all wells, regardless if data is present or not. If turned off (`mark_all=FALSE`, default), only plot wells with data above the omit cutoff.
 #'
 #' @returns ggplot2::geom_tile()
 #' @export
 #'
 #' @examples
 #' #To add
-qcplot.plate_heatmap_readcount = function(ps_obj, omit_cutoff = 100, transform="identity"){
+qcplot.plate_heatmap_readcount = function(
+    ps_obj, omit_cutoff = 100, transform="identity",
+    mark_controls=FALSE, mark_data=FALSE, all_wells=FALSE){
   # plot top taxa over the plate layout
   sample_metadata = clean.ps_sample_sheet(ps_obj)
 
   tot_count = data.frame("total_reads" = phyloseq::sample_sums(ps_obj))
 
   xx_p = merge(sample_metadata, tot_count, by=0,all.x = T)
-  xx_p = xx_p[xx_p$total_reads >= omit_cutoff,]
 
-  # xx_p$total_reads[xx_p$total_reads < omit_cutoff] = 0
+
+  if (all_wells){
+    xx_p$total_reads[xx_p$total_reads < omit_cutoff] = NA
+  } else {
+    xx_p = xx_p[xx_p$total_reads >= omit_cutoff,]
+  }
   # Check if there is data remaining
   if (nrow(xx_p) == 0){
     errorCondition("No data. Try lowering the omit_cutoff, or check your input object.")
@@ -242,13 +251,42 @@ qcplot.plate_heatmap_readcount = function(ps_obj, omit_cutoff = 100, transform="
     ggplot2::aes(x = .data$WELL_COLUMN, y=.data$WELL_ROW, fill=.data$total_reads)) +
     ggplot2::geom_tile() +
     ggplot2::facet_wrap(~.data$PLATE_NOTES, ncol = 3) +
-    ggplot2::scale_fill_gradient(high="red", low="lightblue", transform=transform) +
+    ggplot2::scale_fill_gradient(high="black", low="white", transform=transform, na.value = "white") +
     ggplot2::theme_classic() +
     ggplot2::scale_y_discrete(limits = rev(levels(sample_metadata$WELL_ROW))) +
     ggplot2::scale_x_discrete(limits = levels(sample_metadata$WELL_COLUMN)) +
     ggplot2::ggtitle(paste0("Number of reads (",transform,") per well"),
             subtitle = paste0("Colors correspond total read number (",transform,"). Samples with < ", omit_cutoff, " reads are omitted")
     )
+
+
+  mark_pal = colorspace::qualitative_hcl(length(unique(sample_metadata$ANALYSE_TYPE)))
+  names(mark_pal) = unique(sample_metadata$ANALYSE_TYPE)
+
+  if (mark_data){
+    plate_layout_plot = plate_layout_plot +
+      ggplot2::geom_tile(
+        data=xx_p[xx_p$ANALYSE_TYPE_CODE == "999",],
+        ggplot2::aes(x = .data$WELL_COLUMN, y=.data$WELL_ROW, color=.data$ANALYSE_TYPE),
+        fill=NA, size=0.5) +
+      ggplot2::scale_color_manual(values = mark_pal)
+    }
+
+  if (mark_controls){
+    suppressMessages({
+      plate_layout_plot = plate_layout_plot +
+        ggplot2::geom_tile(
+          data=xx_p[xx_p$ANALYSE_TYPE_CODE != "999",],
+          ggplot2::aes(
+            x = .data$WELL_COLUMN,
+            y=.data$WELL_ROW,
+            color=.data$ANALYSE_TYPE),
+          fill=NA, size=0.65) +
+        ggplot2::scale_color_manual(values = mark_pal)
+      }
+      )
+  }
+
 
   return(plate_layout_plot)
 
